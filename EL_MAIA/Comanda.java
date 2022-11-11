a44528c8fa780547d786b5683e9c4444ee335529
Izabela Fonseca <104567223+Izaazz@users.noreply.github.com>
2022-11-11 14:00:03 -0300

    Add files via upload

--- EL_MAIA/Comanda.java
+++ EL_MAIA/Comanda.java
@@ -1,5 +1,39 @@
-package EL_MAIA;
+package Elmaia;
 
 public class Comanda {
+
+    private String consumo;
+    private double valor;
+
+
+    void listarConsumo(String consumo){
+
+        System.out.println(" --- Lista de Consumo --- ");
+        System.out.println(consumo);
+
+    }
+
+    double calcular10Porcento(double valor){
+        return(valor*0.10);
+    }
+
+    double dividirConta(double valor, int nPessoas){
+        return(valor/nPessoas);
+    }
+
+    //Getters & Setters
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public String getConsumo() {
+        return consumo;
+    }
+    
+    public void setConsumo(String consumo) {
+        this.consumo = consumo;
+    }
     
 }
