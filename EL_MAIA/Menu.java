f2524bc6ab038bc597d87c7a23df14d086b2019a
Gabriela Costa <104565453+Sonasterm@users.noreply.github.com>
2022-11-14 14:51:54 -0300

    Update Menu.java

--- EL_MAIA/Menu.java
+++ EL_MAIA/Menu.java
@@ -23,14 +23,20 @@ public class Menu
         {
             System.out.printf("\n- - - - CARDÁPIO - - - -\n");
             System.out.printf("\nBebidas");
-            System.out.printf("\n[1] Água 500ml...................................R$ 2,00\n[2] Cerveja 500ml................................R$ 4,50");
-            System.out.printf("\n[3] Coca Cola 1L.................................R$ 5,50\n[4] Suco de Laranja 300ml........................R$ 3,50");
-            System.out.printf("\n[5] Suco de Abacaxi..............................R$ 3,50\n[6] Suco de Uva 300ml............................R$ 3,50");
-            System.out.printf("\n[7] Garrafa de Vinho Tinto.......................R$ 79,90\n");
+            System.out.printf("\n[1] Água Atômica 500ml................................R$ 2,00\n[2] Limonada Suína....................................R$ 4,50");
+            System.out.printf("\n[3] Garrafa Bete de Caco Cola.........................R$ 5,50\n[4] Leite com Polvo Maltino...........................R$ 3,50");
+            System.out.printf("\n[5] Suco de Abaxaqui..................................R$ 3,50\n[6] Suco de Frutas Críticas 300ml.....................R$ 3,50");
+            System.out.printf("\n[7] Leite de Amnésia..................................R$ 79,90\n");
+
             System.out.printf("\nPratos");
-            System.out.printf("\n[11] Filé Miau...................................R$ 2,00\n");
+            System.out.printf("\n[11] Filé Miau........................................R$ 2,00\n[12] Batata com molho Barbie Kill.....................R$ 6,50");
+            System.out.printf("\n[11] Bife alí namesa...................................R$ 2,00\n[12] Macarrão a alho e ódio...........................R$ 6,50");
+            System.out.printf("\n[11] Feijão Torpedo...................................R$ 2,00\n[12] Arroz com carne morrida..........................R$ 6,50\n");
+
             System.out.printf("\nSobremesas");
-            System.out.printf("\n[21] Brisadeiro..................................R$ 8,00\n[22] Tiramisu da Engenharia (esgotado)...........R$ 6,50");
+            System.out.printf("\n[21] X & Queique......................................R$ 8,00\n[22] Tiramisu da Engenharia (esgotado)................R$ 6,50");
+            System.out.printf("\n[21] Pudim de Leite Condenado.........................R$ 8,00\n[22] Pácomê...........................................R$ 6,50");
+            System.out.printf("\n[21] Sonho de Falsa...................................R$ 8,00\n");
         }
     }
 }
