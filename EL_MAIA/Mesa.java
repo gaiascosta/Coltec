b94c26c8584ec5dcd550aa54e0187df689c4fcbf
Izabela Fonseca <104567223+Izaazz@users.noreply.github.com>
2022-11-18 15:11:31 -0300

    Update Mesa.java

--- EL_MAIA/Mesa.java
+++ EL_MAIA/Mesa.java
@@ -1,5 +1,6 @@
 package EL_MAIA;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Mesa 
 {
@@ -8,6 +9,8 @@ public class Mesa
 
     // Variáveis
     private int numero_mesa;
+    private String data;
+    private boolean reserva = false;
 
     // Getters and Setters
     public int getNumero_mesa() {
@@ -16,14 +19,12 @@ public class Mesa
     public void setNumero_mesa(int numero_mesa) {
         this.numero_mesa = numero_mesa;
     }
-    private String data;
     public String getData() {
         return data;
     }
     public void setData(String data) {
         this.data = data;
     }
-    private boolean reserva;
     public boolean isReserva() {
         return reserva;
     }
@@ -34,9 +35,40 @@ public class Mesa
     // Funções
     public boolean reservar(int res)
     {
+
+        Scanner esc = new Scanner(System.in);
+        int nPessoas = esc.nextInt();
+        Cliente cliente = new Cliente();
+
         if (res == 1)
         {
             this.reserva = true;
+            
+            if(nPessoas>6){
+                System.out.println(" ---- Máximo de 6 pessoas por mesa :( ----");
+                System.out.println("Gostaria de continuar mesmo assim?\n S/N: ");
+                char c = esc.next().charAt(0);
+                if(c == 'S' || c == 's'){
+                    for(int i = 0; i<6; i++){
+                        System.out.println("Digite o nome do" + (i+1) + "º cliente");
+                        cliente.setNome(esc.next());
+                        cliente.setEmail(esc.next());
+                        this.cliente.add(cliente);
+                    }
+
+                } else{
+                    return(false);
+                }
+
+            } else{
+                for(int i = 0; i<nPessoas; i++){
+                    System.out.println("Digite o nome do" + (i+1) + "º cliente");
+                    cliente.setNome(esc.next());
+                    cliente.setEmail(esc.next());
+                    this.cliente.add(cliente);
+                }
+            }
+
             return(true);
         }
         else if (res == 0)
