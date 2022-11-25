7c9c52f4baf867aaca621d798ca805438db9df72
Gabriela Costa <104565453+Sonasterm@users.noreply.github.com>
2022-11-25 14:20:21 -0300

    Update Data.java

--- EL_MAIA/Data.java
+++ EL_MAIA/Data.java
@@ -1,23 +1,25 @@
 public class Data 
-{   
-    private Comanda comanda = new Comanda();
+{
+    private boolean datinha;
+    public boolean isDatinha() {
+        return datinha;
+    }
+    public void setDatinha(boolean datinha) {
+        this.datinha = datinha;
+    }
     private boolean reserva;
-    private String dataReserva;
-
-    //Getters & Setters
     public boolean isReserva() {
         return reserva;
     }
     public void setReserva(boolean reserva) {
         this.reserva = reserva;
     }
+    private String dataReserva;
     public String getDataReserva() {
         return dataReserva;
     }
     public void setDataReserva(String dataReserva) {
         this.dataReserva = dataReserva;
     }
-    public Comanda getComanda(){
-        return comanda;
-    }
+    Comanda comanda = new Comanda();
 }
