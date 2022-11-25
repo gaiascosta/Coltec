8501152e9e5e282b74374cf2f0d63c326cad2121
Gabriela Costa <104565453+Sonasterm@users.noreply.github.com>
2022-11-25 20:41:40 -0300

    Update Comanda_2.java

--- EL_MAIA/Comanda_2.java
+++ EL_MAIA/Comanda_2.java
@@ -3,31 +3,8 @@ import java.util.ArrayList;
 public class Comanda 
 {
     // Variáveis
-    public double valor;
-
-    // Lista de consumo
+    private double valor;
     ArrayList<String> consumo = new ArrayList<String>();
-
-    // Funções
-    void listarConsumo()
-    {
-        int i = 0;
-        while (i < this.consumo.size())
-        {
-            System.out.println(i+1 + "° - " + this.consumo.get(i));
-            i++;
-        }
-    }
-
-    double calcular10Porcento()
-    {
-        return(this.valor*0.10);
-    }
-
-    double dividirConta(int nPessoas)
-    {
-        return(this.valor/nPessoas);
-    }
 
     //Getters & Setters
     public double getValor() {
@@ -37,6 +14,32 @@ public class Comanda
         this.valor = valor;
     }
 
+    // ----- Funções ------
+
+    // Imprime os itens da lista de consumo
+    public void listarConsumo()
+    {
+        int i = 0;
+        while (i < this.consumo.size())
+        {
+            System.out.println(i+1 + "° - " + this.consumo.get(i));
+            i++;
+        }
+    }
+
+    // Calcula 10% das comanda
+    public double calcular10Porcento()
+    {
+        return(this.valor*0.10);
+    }
+
+    // Divide o valor da conta pelo número de pessoas
+    public double dividirConta(int nPessoas)
+    {
+        return(this.valor/nPessoas);
+    }
+
+    //Adiciona um item na lista de consumo
     public void addItem(String Item){
         this.consumo.add(Item);
     }
