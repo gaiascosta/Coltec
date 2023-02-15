640ed92e0f5e225e9a76bf3654c72a4c30843df3
Gabriela Costa <104565453+gaiascosta@users.noreply.github.com>
2023-02-15 14:51:09 -0300

    Update TopDownMove.cs

--- Scripts/TopDownMove.cs
+++ Scripts/TopDownMove.cs
@@ -15,6 +15,15 @@ public class TopDownMove : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+        {
+            movement.y = 0;
+        }
+        else
+        {
+            movement.x = 0;
+        }
+
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
