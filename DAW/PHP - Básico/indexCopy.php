63db9dbd49951dd3de0737ff2fd7c12091cc1d4a
Gabriela Costa <104565453+gaiascosta@users.noreply.github.com>
2023-06-02 10:56:43 -0300

    Novo arquivo PHP.

---
+++ DAW/PHP - Básico/indexCopy.php
@@ -0,0 +1,62 @@
+<?php
+            $perguntas[] = "Quem compôs o Hino da Indepêndencia?";
+            $alt[0][] = "Dom Pedro I.";
+            $alt[0][] = "Manuel Bandeira";
+            $alt[0][] = "Castro Alves";
+            $alt[0][] = "Carlos Gomes";
+            $respostas[] = 0;
+
+            $perguntas[] = "Que animal da fauna brasileira está retratado na nota de dez reais?";
+            $alt[0][] = "Jabuti";
+            $alt[0][] = "Onça";
+            $alt[0][] = "Arara";
+            $alt[0][] = "Tucano";
+            $respostas[] = 2;
+
+            $perguntas[] = "Quando começou e terminou a primeira guera mundial?";
+            $alt[0][] = "1914 - 1918";
+            $alt[0][] = "1902 - 1908";
+            $alt[0][] = "1920 - 1930";
+            $alt[0][] = "1912 - 1915";
+            $respostas[] = 3;
+
+            $perguntas[] = "Quantos noves tem de 0 a 100?";
+            $alt[0][] = "10";
+            $alt[0][] = "11";
+            $alt[0][] = "20";
+            $alt[0][] = "21";
+            $respostas[] = 2;
+
+            $perguntas[] = "Qual tipo de pedra foi usada na construção da estutua do Cristo Redentor?";
+            $alt[0][] = "Ardósia";
+            $alt[0][] = "Mármore";
+            $alt[0][] = "Granito";
+            $alt[0][] = "Pedra-Sabão";
+            $respostas[] = 3;
+        ?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    </h1>
+    <p>Choose your favorite Web language:</p>
+
+    <form>
+        <input type="radio" name="resposta1" value="HTML">
+        <label for="html">HTML</label><br>
+        <input type="radio" name="resposta2" value="CSS">
+        <label for="css">CSS</label><br>
+        <input type="radio" name="resposta3" value="JavaScript">
+        <label for="javascript">JavaScript</label>
+        <input type="radio" name="resposta4" value="JavaScript">
+        <label for="javascript">JavaScript</label>
+    </form> 
+</body>
+</html>
